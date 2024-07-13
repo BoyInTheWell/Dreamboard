4d1faddb921c9cbc38a633e1339f52aee2b5a9ef
Aurum <rockbarii2@gmail.com>
2024-07-12 21:25:18 -0300

    Added drop stuff

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
 function allowDrop(e){
     e.preventDefault();
 }
 
 function dragEventHandler(e){
     e.dataTransfer.setData('drop_type', e.target.id);
+    
     simulateMouseUp('container')
 }
 
 function dropEventHandler (e) {
     e.preventDefault();
     const data = e.dataTransfer.getData('drop_type')
-    console.log(data);
+    const canvas = document.getElementById('canvas');
+    let htmlNode = document.createElement('div');
+    htmlNode.style.top = e.pageY + 'px';
+    htmlNode.style.left = e.pageX + 'px';
+    htmlNode.className = "canvas-element"
+    htmlNode.innerHTML = `
+    <div class="canvas-element-note">
+        This is a ${data} at X: ${e.pageX} - Y: ${e.pageY}
+        ${JSON.stringify(canvas.getBoundingClientRect())}
+    </div>
+    `
+    canvas.appendChild(htmlNode);
+    console.log(canvas.getBoundingClientRect());
+    console.log('X: ' + e.pageX + ' - Y:' + e.pageY);
+    console.log('ScrollTop: ' + pos.top + ' - ScrollLeft:' + pos.left);
 }
 
 function simulateMouseUp(id) {
@@ -27,14 +44,12 @@ function domContentLoadedHandler() {
     const ele = document.getElementById('container');
     ele.style.cursor = 'grab';
 
-    const canvas = document.getElementById('canvas')
+    const canvas = document.getElementById('canvas');
 
     const rightBoundRect = canvas.getBoundingClientRect().right / 8;
     const bottomBoundRect = canvas.getBoundingClientRect().bottom / 8;
 
     ele.scrollTo(rightBoundRect * 3, bottomBoundRect * 3);
-
-    let pos = { top: 0, left: 0, x: 0, y: 0 };
 
     const mouseDownHandler = function (e) {
         ele.style.cursor = 'grabbing';
